2cbd776d630420a0ff6bde1b063ae315f1be6382
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2023-02-09 12:47:52 +0100

    Install react-firebase-hooks to connect to firestore

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,10 +1,20 @@
 "use client";
+import { useCollection } from "react-firebase-hooks/firestore";
 
 import NewChat from "./NewChat";
 import { useSession, signOut } from "next-auth/react";
+import { db } from "../firebase";
+import { collection } from "firebase/firestore";
 
 const SideBar = () => {
   const { data: session } = useSession();
+
+  const [chats, loading, error] = useCollection(
+    session && collection(db, "users", session?.user?.email!, "chats")
+  );
+
+  console.log(chats);
+
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className="flex-1">
